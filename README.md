69f18fdd2e63e95f472deaf4ed889906c1ae0cc3
sinmetal <metal.tie@gmail.com>
2018-01-23 12:00:20 +0900

    https://phaser.io/examples/v2/tilemaps/paint-tiles を動かしてみた

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # phaser-playground
+
+https://phaser.io/examples/v2/tilemaps/paint-tiles を実際に動かしてみたサンプル。
+TypeScriptに書き換えているのと、 https://github.com/photonstorm/phaser-ce を利用するようにちょっとだけ修正している。
+
+## Build
+
+```
+tsc
+```
+
+## Run
+
+```
+goapp serve .
+```
--- app.ts
+++ app.ts
@@ -1,24 +1,83 @@
 class SimpleGame {
-
     constructor() {
-        this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', { preload: this.preload, create: this.create });
+        this.game = new Phaser.Game(1280, 832, Phaser.CANVAS, 'content', { preload: this.preload, create: this.create, update: this.update, render: this.render });
     }
 
     game: Phaser.Game;
+    map: any;
+    layer: any;
+
+    marker: any;
+    currentTile: any;
+    cursors: any;
 
     preload() {
-        this.game.load.image('logo', 'phaser2.png');
+        this.game.load.tilemap('desert', 'assets/tilemaps/maps/desert.json', null, Phaser.Tilemap.TILED_JSON);
+        this.game.load.image('tiles', 'assets/tilemaps/tiles/tmw_desert_spacing.png');
     }
 
     create() {
-        var logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'logo');
-        logo.anchor.setTo(0.5, 0.5);
+        this.map = this.game.add.tilemap('desert');
+
+        this.map.addTilesetImage('Desert', 'tiles');
+
+        this.currentTile = this.map.getTile(2, 3);
+
+        this.layer = this.map.createLayer('Ground');
+
+        this.layer.resizeWorld();
+
+        this.marker = this.game.add.graphics();
+        this.marker.lineStyle(2, 0x000000, 1);
+        this.marker.drawRect(0, 0, 32, 32);
+
+        this.cursors = this.game.input.keyboard.createCursorKeys();
+    }
+
+    update() {
+        this.marker.x = this.layer.getTileX(this.game.input.activePointer.worldX) * 32;
+        this.marker.y = this.layer.getTileY(this.game.input.activePointer.worldY) * 32;
+
+        if (this.game.input.mousePointer.isDown)
+        {
+            if (this.game.input.keyboard.isDown(Phaser.Keyboard.SHIFT))
+            {
+                this.currentTile = this.map.getTile(this.layer.getTileX(this.marker.x), this.layer.getTileY(this.marker.y));
+            }
+            else
+            {
+                if (this.map.getTile(this.layer.getTileX(this.marker.x), this.layer.getTileY(this.marker.y)).index != this.currentTile.index)
+                {
+                    this.map.putTile(this.currentTile, this.layer.getTileX(this.marker.x), this.layer.getTileY(this.marker.y));
+                }
+            }
+        }
+
+        if (this.cursors.left.isDown)
+        {
+            this.game.camera.x -= 4;
+        }
+        else if (this.cursors.right.isDown)
+        {
+            this.game.camera.x += 4;
+        }
+
+        if (this.cursors.up.isDown)
+        {
+            this.game.camera.y -= 4;
+        }
+        else if (this.cursors.down.isDown)
+        {
+            this.game.camera.y += 4;
+        }
+    }
+
+    render() {
+        this.game.debug.text('Left-click to paint. Shift + Left-click to select tile. Arrows to scroll.', 32, 32, '#efefef');
     }
 
 }
 
 window.onload = () => {
-
-    var game = new SimpleGame();
-
+    const game = new SimpleGame();
 };
