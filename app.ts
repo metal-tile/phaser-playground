bf5ff9c5c2aac00bbaa3189b7077841bc384619f
sinmetal <metal.tie@gmail.com>
2018-02-20 00:49:19 +0900

    キャラクターが歩く機能を追加

--- app.ts
+++ app.ts
@@ -10,10 +10,14 @@ class SimpleGame {
     marker: any;
     currentTile: any;
     cursors: any;
+    wKey: any;
+
+    sprite: any;
 
     preload() {
         this.game.load.tilemap('desert', 'assets/tilemaps/maps/desert.json', null, Phaser.Tilemap.TILED_JSON);
         this.game.load.image('tiles', 'assets/tilemaps/tiles/tmw_desert_spacing.png');
+        this.game.load.spritesheet('zet', 'assets/character/zet.png', 32, 48);
     }
 
     create() {
@@ -31,10 +35,26 @@ class SimpleGame {
         this.marker.lineStyle(2, 0x000000, 1);
         this.marker.drawRect(0, 0, 32, 32);
 
+        this.sprite = this.game.add.sprite(96, 192, 'zet');
+
+        this.sprite.animations.add('walk', [0, 1, 2]);
+        this.sprite.animations.play('walk', 6, true);
+        this.sprite.scale.set(1);
+        this.sprite.anchor.setTo(0.5, 0.5);
+        this.game.physics.enable(this.sprite);
+        this.game.camera.follow(this.sprite);
+
         this.cursors = this.game.input.keyboard.createCursorKeys();
+        this.wKey = this.game.input.keyboard.addKey(Phaser.Keyboard.W);
     }
 
     update() {
+        this.game.physics.arcade.collide(this.sprite, this.layer);
+
+        this.sprite.body.velocity.x = 0;
+        this.sprite.body.velocity.y = 0;
+        this.sprite.body.angularVelocity = 0;
+
         this.marker.x = this.layer.getTileX(this.game.input.activePointer.worldX) * 32;
         this.marker.y = this.layer.getTileY(this.game.input.activePointer.worldY) * 32;
 
@@ -55,20 +75,23 @@ class SimpleGame {
 
         if (this.cursors.left.isDown)
         {
-            this.game.camera.x -= 4;
+            // this.game.camera.x -= 4;
+
         }
         else if (this.cursors.right.isDown)
         {
             this.game.camera.x += 4;
         }
 
-        if (this.cursors.up.isDown)
+        if (this.wKey.isDown)
         {
-            this.game.camera.y -= 4;
+            // this.game.camera.y -= 4;
+            this.sprite.body.velocity.y = -100;
         }
         else if (this.cursors.down.isDown)
         {
-            this.game.camera.y += 4;
+            // this.game.camera.y += 4;
+            this.sprite.body.velocity.y = 100;
         }
     }
 
